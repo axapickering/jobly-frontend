252dd65eb60c5016d574f59bbea6935bcb89d7f1
Akhaylin Rajagopaul <akhaylin.r@gmail.com>
2023-11-08 17:51:39 -0500

    Formatted salary finished todo's

--- src/JobCard.js
+++ src/JobCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import {addCommasToIntegerPart} from './helpers'
 /**
  *  Renders one card with info about a job
  *
  * Props: job - object containing all info about job
  *
  *  JobsList -> JobCard
- */ 
+ */
 function JobCard({ title, companyHandle, salary, equity }) {
 
   if (companyHandle) {
@@ -17,13 +17,29 @@ function JobCard({ title, companyHandle, salary, equity }) {
     .join("-");
 
   }
+  function addCommasToIntegerPart(integerPart) {
+    let strinInt = String(integerPart)
+    let result = '';
+    let length = strinInt.length;
+
+    for (let i = length - 1; i >= 0; i--) {
+      let position = length - 1 - i;
+
+      if (position > 0 && position % 3 === 0) {
+        result = ',' + result;
+      }
+
+      result = strinInt[i] + result;
+    }
+    return ` $${result}`;
+  }
 
   return (
     <div class="card w-75 mb-3" >
       <div class="card-body">
         <p><strong>{title}</strong></p>
           <h5 class="card-title">{companyHandle}</h5>
-          <p class="card-text">Salary:{salary}</p>
+          <p class="card-text">Salary:{addCommasToIntegerPart(salary)}</p>
           <p class="float-right">Equity: {equity}</p>
       </div>
     </div>
--- src/RouteList.js
+++ src/RouteList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import HomePage from "./HomePage";
 import CompaniesPage from "./CompaniesPage";
 import CompanyDetail from "./CompanyDetail";
@@ -19,6 +19,7 @@ function RouteList() {
       <Route element={<CompaniesPage />} path="/companies" />
       <Route element={<CompanyDetail />} path="/companies/:handle" />
       <Route element={<JobsPage />} path="/jobs" />
+      <Route element={<Navigate to="/"/>} path="*"/>
     </Routes>
   );
 }
