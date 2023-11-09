e60a9fee843a1f7b0d6d0277edb297725f873f94
Akhaylin Rajagopaul <akhaylin.r@gmail.com>
2023-11-08 20:50:05 -0500

    Added context skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import { BrowserRouter } from 'react-router-dom';
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 import Nav from './navigation/Nav';
 import RouteList from './navigation/RouteList';
+import userContext from './context/userContext';
 
 /**Renders App
  * App -> {Nav, RouteList}
  */
 function App() {
+  const [user, setUser] =  useState()
+  //TODO:Login Call to API and set state function
+
   return (
-    <BrowserRouter>
-    <Nav />
-    <div className='App container'>
-      <div className='row'>
-        <div className='col-11'>
-        <RouteList />
+    <userContext.Provider value={user}>
+      <BrowserRouter>
+      <Nav />
+      <div className='App container'>
+        <div className='row'>
+          <div className='col-11'>
+          <RouteList />
+          </div>
         </div>
       </div>
-    </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </userContext.Provider>
   )
 }
 
---
+++ src/context/userContext.js
@@ -0,0 +1,5 @@
+import { createContext } from React;
+
+const userContext = createContext();
+
+export default userContext;
